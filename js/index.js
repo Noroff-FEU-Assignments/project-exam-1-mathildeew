5ddfd62de510092d9ab1484029e49fecb8648072
mathildeew <mathildewiik@hotmail.com>
2022-08-31 22:04:45 +0200

    sdf

--- js/index.js
+++ js/index.js
@@ -1,26 +1,44 @@
 import { errorMessage } from "./ui/errorMessage.js";
 
-const baseUrl = "https://projects.mathildeelinor.no/wp-json/wp/v2/posts";
+const baseUrl = "https://projects.mathildeelinor.no/wp-json/wp/v2/posts?_embed";
 
 const loader = document.querySelector(".loader");
 const blogGrid = document.querySelector(".blog-grid");
 
 async function getBlogPosts(url) {
-  const response = await fetch(url);
-  const posts = await response.json();
-  console.log(posts);
+  try {
+    const response = await fetch(url);
+    const posts = await response.json();
+    console.log(posts);
 
-  posts.forEach(function (posts) {
-    loader.style.display = "none";
-    blogGrid.innerHTML += `
-    <div class="post-card">
+    const date = new Date(posts[0].date).toLocaleDateString("utc", {
+      year: "numeric",
+      month: "long",
+      day: "2-digit",
+    });
+
+    for (let i = 0; i < posts.length; i++) {
+      loader.style.display = "none";
+
+      if (i === 4) {
+        break;
+      }
+      blogGrid.innerHTML += `
+    <div class="posts">
+    <a href="blogpost.html?id=${posts[i].id}">
+      <div class="post-card" style="background-image: url('${posts[i]._embedded["wp:featuredmedia"]["0"].source_url}')">
         <div class="posts-card-background">
-            <h2>${posts.title.rendered}</h2>
-            <img src="" alt=""/>
+          <h2>${posts[i].title.rendered}</h2>
+          <h3>${date}</h3>
         </div>
-    </div>
+      </div>
+    </a>
+  </div>
     `;
-  });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 getBlogPosts(baseUrl);

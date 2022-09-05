ea84ab2500bba93d42ac905aab8bdd7c508c64cb
mathildeew <mathildewiik@hotmail.com>
2022-09-05 18:12:17 +0200

    sdf

--- js/blog.js
+++ js/blog.js
@@ -1,15 +1,10 @@
 const blogGrid = document.querySelector(".blog-grid2");
 const selectCategories = document.querySelector(".categories");
+const showMore = document.querySelector(".show-more");
 const select = document.querySelector("select");
 
 function blog(posts, categories) {
   document.title += " | Blog";
-
-  const date = new Date(posts[0].date).toLocaleDateString("utc", {
-    year: "numeric",
-    month: "long",
-    day: "2-digit",
-  });
 
   // Filter by category
   categories.forEach(function (categories) {
@@ -18,23 +13,26 @@ function blog(posts, categories) {
   `;
   });
 
-  select.onchange = function () {};
+  for (let i = 0; i < posts.length; i++) {
+    const date = new Date(posts[i].date).toLocaleDateString("utc", {
+      year: "numeric",
+      month: "long",
+      day: "2-digit",
+    });
 
-  // Display blogposts
-  posts.forEach(function (posts) {
     blogGrid.innerHTML += `
         <div class="posts">
-          <a href="blogpost.html?id=${posts.id}">
-            <div class="post-card" style="background-image: url('${posts._embedded["wp:featuredmedia"]["0"].source_url}')">
+          <a href="blogpost.html?id=${posts[i].id}">
+            <div class="post-card" style="background-image: url('${posts[i]._embedded["wp:featuredmedia"]["0"].source_url}')">
               <div class="posts-card-background">
-                <h2>${posts.title.rendered}</h2>
+                <h2>${posts[i].title.rendered}</h2>
                 <h3>${date}</h3>
               </div>
             </div>
           </a>
         </div>
         `;
-  });
+  }
 }
 
 getApi();

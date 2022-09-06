12d4e61e56a5b394558636ffb665c45a3e7ca1ee
mathildeew <mathildewiik@hotmail.com>
2022-09-06 21:50:09 +0200

    no progress

--- js/blog.js
+++ js/blog.js
@@ -7,10 +7,10 @@ function blog(posts, categories) {
   document.title += " | Blog";
 
   function displayPosts() {
-    let currentPosts = 0;
-    showPosts = posts.slice(currentPosts, currentPosts + 4);
+    // posts = posts.slice(currentPosts, currentPosts + 4);
+    console.log(posts);
 
-    showPosts.forEach(function (posts) {
+    posts.forEach(function (posts) {
       const date = new Date(posts.date).toLocaleDateString("utc", {
         year: "numeric",
         month: "long",
@@ -18,32 +18,34 @@ function blog(posts, categories) {
       });
 
       blogGrid.innerHTML += `
-  <div class="posts">
-        <a href="blogpost.html?id=${posts.id}">
-          <div class="post-card" style="background-image: url('${posts._embedded["wp:featuredmedia"]["0"].source_url}')">
-            <div class="posts-card-background">
-              <h2>${posts.title.rendered}</h2>
-              <h3>${date}</h3>
-            </div>
-          </div>
-        </a>
-      </div>
-  `;
+                            <div class="posts">
+                              <a href="blogpost.html?id=${posts.id}">
+                              <div class="post-card" style="background-image: url('${posts._embedded["wp:featuredmedia"]["0"].source_url}')">
+                                <div class="posts-card-background">
+                                  <h2>${posts.title.rendered}</h2>
+                                  <h3>${date}</h3>
+                                </div>
+                              </div>
+                              </a>
+                            </div>
+                            `;
     });
+
     // Show more posts
     const displayMoreposts = () => {
-      displayPosts(showPosts.push(currentPosts + 4));
+      let currentPosts = post.slice(0, 4);
+
       if (!(currentPosts + 4 > posts.length)) {
-        currentPosts += 4;
+        posts += 4;
       }
-      // if (currentPosts === posts.length) {
-      //   showMoreButton.removeEventListener("click", displayMoreposts);
-      //   showMoreButton.display.style = "none";
-      // }
-      console.log(currentPosts);
+      if (currentPosts === posts.length) {
+        showMoreButton.removeEventListener("click", displayMoreposts);
+        showMoreButton.display.style = "none";
+      }
     };
     showMoreButton.addEventListener("click", displayMoreposts);
   }
+
   displayPosts();
 
   // Filter by category
@@ -59,29 +61,3 @@ function blog(posts, categories) {
 }
 
 getApi();
-
-// let currentPosts = 0;
-// const displayMoreposts = (posts) => {
-//   displayPosts(posts.slice(currentPosts, currentPosts + 4));
-//   if (!(currentPosts + 4 > posts.lenght)) {
-//     currentPosts += 4;
-//   }
-//   if (currentPosts === posts.lenght) {
-//     showMoreButton.removeEventListener("click", displayMoreposts);
-//     showMoreButton.display.style = "none";
-//   }
-// };
-
-// displayMoreposts(posts);
-// showMoreButton.addEventListener("click", displayMoreposts);
-// }
-// }
-
-// displayPosts();
-
-// Display blogposts to show 5 first
-//  function displayPosts() {
-//   let currentPosts = 5;
-//   let slicedPosts = posts.slice(0, currentPosts);
-
-//   console.log(slicedPosts);

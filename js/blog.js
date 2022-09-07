3bd4b493f61e87890b384f6f478ce558ebf5740b
mathildeew <mathildewiik@hotmail.com>
2022-09-08 00:38:57 +0200

    carousel doesnt work

--- js/blog.js
+++ js/blog.js
@@ -1,47 +1,104 @@
 const selectCategories = document.querySelector(".categories");
-const showMoreButton = document.querySelector(".show-more");
+const showMoreBtn = document.querySelector(".showmore-button");
 const select = document.querySelectorAll("select");
 const blogGridPosts = document.querySelector(".blog-grid-posts");
 
+// function blog(posts, categories) {
+//   document.title += " | Blog";
+
+//   // Show more posts
+//   displayPosts(posts);
+
+//   // Filter by category
+//   categories.forEach(function (categories) {
+//     selectCategories.innerHTML += `
+//                             <option>${categories.name}</option>
+//   `;
+
+//     selectCategories.onchange = (event) => {
+//       // code to change category
+//     };
+//   });
+// }
+
+// getApi();
+
+// function displayPosts(posts) {
+//   posts.forEach((posts) => {
+//     const date = new Date(posts.date).toLocaleDateString("utc", {
+//       year: "numeric",
+//       month: "long",
+//       day: "2-digit",
+//     });
+
+//     blogGridPosts.innerHTML += `
+//                             <div class="posts">
+//                               <a href="blogpost.html?id=${posts.id}">
+//                               <div class="post-card" style="background-image: url('${posts._embedded["wp:featuredmedia"]["0"].source_url}')">
+//                                 <div class="posts-card-background">
+//                                   <h2>${posts.title.rendered}</h2>
+//                                   <h3>${date}</h3>
+//                                 </div>
+//                               </div>
+//                             </a>
+//                           </div>
+//                             `;
+//   });
+// }
+
 function blog(posts, categories) {
-  document.title += " | Blog";
+  let pageIndex = 0;
+  const postsPerPage = 4;
 
-  // Show more posts
-  displayPosts(posts);
+  function loadPosts() {
+    for (
+      let i = pageIndex * postsPerPage;
+      i < pageIndex * postsPerPage + postsPerPage;
+      i++
+    ) {
+      const date = new Date(posts[0].date).toLocaleDateString("utc", {
+        year: "numeric",
+        month: "long",
+        day: "2-digit",
+      });
 
-  // Filter by category
-  categories.forEach(function (categories) {
-    selectCategories.innerHTML += `
-                            <option>${categories.name}</option>
-  `;
+      if (!posts[i]) {
+        break;
+      }
 
-    selectCategories.onchange = (event) => {
-      // code to change category
-    };
-  });
+      blogGridPosts.innerHTML += `
+                                  <a href="blogpost.html?id=${posts[i].id}">
+                                    <div class="post-card" style="background-image: url('${posts[i]._embedded["wp:featuredmedia"]["0"].source_url}')">
+                                      <div class="posts-card-background">
+                                        <h2>${posts[i].title.rendered}</h2>
+                                        <h3>${date}</h3>
+                                      </div>
+                                    </div>
+                                  </a>
+                                
+
+      `;
+      // blogGridHome.append(postDisplayed);
+    }
+    loadPageNav();
+  }
+  function loadPageNav() {
+    // previous.innerHTML = "";
+
+    for (let i = 0; i < posts.length / postsPerPage; i++) {
+      showMoreBtn.addEventListener("click", (e) => {
+        pageIndex = e.target.innerHTML + 1;
+        loadPosts(posts);
+        console.log(pageIndex);
+      });
+
+      if (i === pageIndex) {
+        // showMoreBtn.style.display = "none";
+      }
+    }
+  }
+  loadPosts();
+  console.log(posts);
 }
 
 getApi();
-
-function displayPosts(posts) {
-  posts.forEach((posts) => {
-    const date = new Date(posts.date).toLocaleDateString("utc", {
-      year: "numeric",
-      month: "long",
-      day: "2-digit",
-    });
-
-    blogGridPosts.innerHTML += `
-                            <div class="posts">
-                              <a href="blogpost.html?id=${posts.id}">
-                              <div class="post-card" style="background-image: url('${posts._embedded["wp:featuredmedia"]["0"].source_url}')">
-                                <div class="posts-card-background">
-                                  <h2>${posts.title.rendered}</h2>
-                                  <h3>${date}</h3>
-                                </div>
-                              </div>
-                            </a>
-                          </div>
-                            `;
-  });
-}
--- js/index.js
+++ js/index.js
@@ -1,96 +1,136 @@
 const blogGridHome = document.querySelector(".blog-grid");
 const showMoreButton = document.querySelector(".show-more");
 const navnav = document.querySelector(".navnav");
-const previous = document.querySelector(".previous");
-const next = document.querySelector(".next");
+const previousBtn = document.getElementById("prev-button");
+const nextBtn = document.getElementById("next-button");
+const postsDisplayed = document.querySelector(".carousel");
 
-// function index(posts) {
-//   for (let i = 0; i < posts.length; i++) {
-//     const date = new Date(posts[0].date).toLocaleDateString("utc", {
-//       year: "numeric",
-//       month: "long",
-//       day: "2-digit",
-//     });
-
-//     if (i === 4) {
-//       break;
-//     }
-
-//     blogGridHome.innerHTML += `
-//                                 <a href="blogpost.html?id=${posts[i].id}">
-//                                 <div class="post-card" style="background-image: url('${posts[i]._embedded["wp:featuredmedia"]["0"].source_url}')">
-//                                   <div class="posts-card-background">
-//                                     <h2>${posts[i].title.rendered}</h2>
-//                                     <h3>${date}</h3>
-//                                   </div>
-//                                 </div>
-//                                 </a>
-//                               `;
-//   }
-// }
-
-// getApi();
-
-// ALT UNDER HER ER TEST - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+let postsPerPage = 4;
+let currentPage = 1;
 
 function index(posts) {
-  let pageIndex = 0;
-  let postsPerPage = 4;
+  //  DETTE FUNGERER to display posts!! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  // for (let i = 0; i < posts.length; i++) {
+  //   const date = new Date(posts[0].date).toLocaleDateString("utc", {
+  //     year: "numeric",
+  //     month: "long",
+  //     day: "2-digit",
+  //   });
+  //   if (i === 4) {
+  //     break;
+  //   }
+  //   blogGridHome.innerHTML += `
+  //                               <a href="blogpost.html?id=${posts[i].id}">
+  //                               <div class="post-card" style="background-image: url('${posts[i]._embedded["wp:featuredmedia"]["0"].source_url}')">
+  //                                 <div class="posts-card-background">
+  //                                   <h2>${posts[i].title.rendered}</h2>
+  //                                   <h3>${date}</h3>
+  //                                 </div>
+  //                               </div>
+  //                               </a>
+  //                             `;
+  //  DETTE FUNGERER halveis!! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  // let pageIndex = 0;
+  // const postsPerPage = 4;
+  // function loadPosts() {
+  //   for (
+  //     let i = pageIndex * postsPerPage;
+  //     i < pageIndex * postsPerPage + postsPerPage;
+  //     i++
+  //   ) {
+  //     const date = new Date(posts[0].date).toLocaleDateString("utc", {
+  //       year: "numeric",
+  //       month: "long",
+  //       day: "2-digit",
+  //     });
+  //     if (!posts[i]) {
+  //       break;
+  //     }
+  //     blogGridHome.innerHTML = `
+  //                                 <a href="blogpost.html?id=${posts[i].id}">
+  //                                   <div class="post-card" style="background-image: url('${posts[i]._embedded["wp:featuredmedia"]["0"].source_url}')">
+  //                                     <div class="posts-card-background">
+  //                                       <h2>${posts[i].title.rendered}</h2>
+  //                                       <h3>${date}</h3>
+  //                                     </div>
+  //                                   </div>
+  //                                 </a>
+  //     `;
+  //     // blogGridHome.append(postDisplayed);
+  //   }
+  //   loadPageNav();
+  // }
+  // function loadPageNav() {
+  //   navnav.innerHTML = "";
+  //   // previous.innerHTML = "";
+  //   for (let i = 0; i < posts.length / postsPerPage; i++) {
+  //     const span = document.createElement("span");
+  //     span.innerHTML = i + 1;
+  //     span.addEventListener("click", (e) => {
+  //       pageIndex = e.target.innerHTML - 1;
+  //       loadPosts();
+  //       console.log(pageIndex);
+  //     });
+  //     if (i === pageIndex) {
+  //       span.style.fontSize = "2rem";
+  //     }
+  //     navnav.append(span);
+  //   }
+  // }
+  // loadPosts();
 
-  loadPosts();
+  // Create HTML Grid
+  let postDisplayed = "";
+  console.log(posts);
 
-  function loadPosts() {
-    for (
-      let i = pageIndex * postsPerPage;
-      i < pageIndex * postsPerPage + postsPerPage;
-      i++
-    ) {
+  posts.filter((post, index) => {
+    let start = (currentPage - 1) * postsPerPage;
+    let end = currentPage * postsPerPage;
+
+    if (index >= start && index < end) return true;
+  });
+
+  function displayPosts(page = 1) {
+    if (page == 1) {
+      previousBtn.style.display = "none";
+    } else {
+      previousBtn.style.display = "block";
+    }
+
+    for (let i = 0; i < posts.length; i++) {
       const date = new Date(posts[0].date).toLocaleDateString("utc", {
         year: "numeric",
         month: "long",
         day: "2-digit",
       });
-
-      if (!posts[i]) {
+      if (i === 4) {
         break;
       }
-
       blogGridHome.innerHTML = `
-                                <a href="blogpost.html?id=${posts[i].id}">
-                                <div class="post-card" style="background-image: url('${posts[i]._embedded["wp:featuredmedia"]["0"].source_url}')">
-                                  <div class="posts-card-background">
-                                    <h2>${posts[i].title.rendered}</h2>
-                                    <h3>${date}</h3>
-                                  </div>
-                                </div>
-                                </a>
-
-
-      `;
-      // blogGridHome.append(posts);
+                                    <a href="blogpost.html?id=${posts[i].id}">
+                                    <div class="post-card" style="background-image: url('${posts[i]._embedded["wp:featuredmedia"]["0"].source_url}')">
+                                      <div class="posts-card-background">
+                                        <h2>${posts[i].title.rendered}</h2>
+                                        <h3>${date}</h3>
+                                      </div>
+                                    </div>
+                                    </a>
+                                  `;
     }
-    loadPageNav();
-  }
-  function loadPageNav() {
-    navnav.innerHTML = "";
-    // previous.innerHTML = "";
-
-    for (let i = 0; i < posts.length / postsPerPage; i++) {
-      const span = document.createElement("span");
-      span.innerHTML = i + 1;
-      span.addEventListener("click", (e) => {
-        pageIndex = e.target.innerHTML - 1;
-        loadPosts();
-        console.log(pageIndex);
-      });
-
-      if (i === pageIndex) {
-        span.style.fontSize = "2rem";
-      }
-
-      navnav.append(span);
+    function previousPage() {
+      if (currentPage > 1) currentPage--;
+      displayPosts();
     }
+
+    function nextPage() {
+      if (currentPage * postsPerPage < posts.length) currentPage++;
+      displayPosts();
+    }
+
+    previousBtn.addEventListener("click", previousPage, false);
+    nextBtn.addEventListener("click", nextPage, false);
   }
-  console.log(posts);
+  displayPosts();
 }
+
 getApi();

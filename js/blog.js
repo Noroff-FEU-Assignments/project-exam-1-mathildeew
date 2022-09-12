522059b8c889bb7f28f7487f223a864e1341bd6f
mathildeew <mathildewiik@hotmail.com>
2022-09-12 16:20:28 +0200

    carousel and all blogposts navigation works

--- js/blog.js
+++ js/blog.js
@@ -2,24 +2,23 @@ import { errorMessage } from "./ui/errorMessage.js";
 
 const loader = document.querySelector(".loader");
 const selectCategories = document.querySelector(".categories");
-const showMoreBtn = document.querySelector(".showmore-button");
 const select = document.querySelectorAll("select");
-const blogGrid = document.querySelector(".blog-grid-posts");
+const blogPostContainer = document.querySelector(".blogposts");
+const postsNav = document.querySelector(".blogposts-nav");
 
 document.title += " | Blog";
 
-const baseUrl = "https://projects.mathildeelinor.no/wp-json/wp/v2/";
-const firstPosts = "posts/?per_page=6&_embed";
-const secondPosts = "posts/?per_page=12&_embed";
+const baseUrl =
+  "https://projects.mathildeelinor.no/wp-json/wp/v2/posts/?per_page=100&_embed";
 
 async function getBlogPosts() {
   try {
-    const firstBlogPosts = await (await fetch(baseUrl + firstPosts)).json();
-    const secondBlogPosts = await (await fetch(baseUrl + secondPosts)).json();
+    const allBlogPosts = await (await fetch(baseUrl)).json();
     const categories = await (await fetch(baseUrl + "categories")).json();
     loader.style.display = "none";
 
-    displayFirstPosts(firstBlogPosts);
+    displayBlogPosts(allBlogPosts);
+    loadBlogPostNav(allBlogPosts);
 
     filterByCategory(categories);
   } catch (error) {
@@ -30,24 +29,63 @@ async function getBlogPosts() {
 
 getBlogPosts();
 
-function displayFirstPosts(posts) {
-  for (let i = 0; i < posts.length; i++) {
-    const date = new Date(posts[0].date).toLocaleDateString("utc", {
+// Display blogposts
+let pageIndex = 0;
+let postsPerPage = 4;
+
+function displayBlogPosts(blogPosts) {
+  console.log(blogPosts);
+
+  blogPostContainer.innerHTML = "";
+
+  for (
+    let i = pageIndex * postsPerPage;
+    i < pageIndex * postsPerPage + postsPerPage;
+    i++
+  ) {
+    if (!blogPosts[i]) {
+      break;
+    }
+
+    const date = new Date(blogPosts[0].date).toLocaleDateString("utc", {
       year: "numeric",
       month: "long",
       day: "2-digit",
     });
 
-    blogGrid.innerHTML += `
-                            <a href="blogpost.html?id=${posts[i].id}">
-                            <div class="post-card" style="background-image: url('${posts[i]._embedded["wp:featuredmedia"]["0"].source_url}')">
-                              <div class="posts-card-background">
-                                <h2>${posts[i].title.rendered}</h2>
-                                <h3>${date}</h3>
-                              </div>
-                            </div>
-                            </a>
-                        `;
+    const blogPostContent = document.createElement("div");
+    blogPostContent.innerHTML = `
+    <a href="blogpost.html?id=${blogPosts[i].id}">
+    <div class="post-card" style="background-image: url('${blogPosts[i]._embedded["wp:featuredmedia"]["0"].source_url}')">
+      <div class="posts-card-background">
+        <h2>${blogPosts[i].title.rendered}</h2>
+        <h3>${date}</h3>
+      </div>
+    </div>
+    </a>
+    `;
+
+    blogPostContainer.append(blogPostContent);
+  }
+}
+
+// Navigation between blogposts
+function loadBlogPostNav(blogPosts) {
+  postsNav.innerHTML = "";
+
+  for (let i = 0; i < blogPosts.length / postsPerPage; i++) {
+    const span = document.createElement("span");
+    span.innerHTML = i + 1;
+    span.addEventListener("click", (e) => {
+      pageIndex = e.target.innerHTML - 1;
+      displayBlogPosts(blogPosts);
+    });
+
+    // if (i === pageIndex) {
+    //   span.style.fontSize = "3rem";
+    // }
+
+    postsNav.append(span);
   }
 }
 
--- js/index.js
+++ js/index.js
@@ -1,56 +1,110 @@
 import { errorMessage } from "./ui/errorMessage.js";
 
 const loader = document.querySelector(".loader");
-const blogGrid = document.querySelector(".blog-grid");
-const showMoreButton = document.querySelector(".show-more");
-const navnav = document.querySelector(".navnav");
-const previousBtn = document.getElementById("prevbtn");
-const nextBtn = document.getElementById("nextbtn");
-const postsDisplayed = document.querySelector(".carousel");
+const slides = document.querySelectorAll(".blog-grid");
+const slideOne = document.querySelector(".slide-one");
+const slideTwo = document.querySelector(".slide-two");
+const slideThree = document.querySelector(".slide-three");
 
 const baseUrl = "https://projects.mathildeelinor.no/wp-json/wp/v2/posts";
-const firstPosts = baseUrl + "?per_page=4&_embed";
-const secondPosts = baseUrl + "?per_page=4&_embed";
+const latestPost = baseUrl + "?per_page=12&_embed";
 
 // Fetch API data
 async function getLatestPosts() {
   try {
-    const firstSlide = await (await fetch(firstPosts)).json();
-    const secondSlide = aw;
+    const blogPosts = await (await fetch(latestPost)).json();
+    console.log(blogPosts);
     loader.style.display = "none";
 
-    firstPostsSlider(firstSlide);
+    for (let i = 0; i < blogPosts.length; i++) {
+      const date = new Date(blogPosts[0].date).toLocaleDateString("utc", {
+        year: "numeric",
+        month: "long",
+        day: "2-digit",
+      });
+
+      if (i <= 3) {
+        slideOne.innerHTML += `
+                              <a href="blogpost.html?id=${blogPosts[i].id}">
+                              <div class="post-card" style="background-image: url('${blogPosts[i]._embedded["wp:featuredmedia"]["0"].source_url}')">
+                                <div class="posts-card-background">
+                                  <h2>${blogPosts[i].title.rendered}</h2>
+                                  <h3>${date}</h3>
+                                </div>
+                              </div>
+                              </a>
+                            `;
+      } else if (i <= 7) {
+        slideTwo.innerHTML += `
+                              <a href="blogpost.html?id=${blogPosts[i].id}">
+                              <div class="post-card" style="background-image: url('${blogPosts[i]._embedded["wp:featuredmedia"]["0"].source_url}')">
+                                <div class="posts-card-background">
+                                  <h2>${blogPosts[i].title.rendered}</h2>
+                                  <h3>${date}</h3>
+                                </div>
+                              </div>
+                              </a>
+                              `;
+      } else if (i <= 11) {
+        slideThree.innerHTML += `
+                                <a href="blogpost.html?id=${blogPosts[i].id}">
+                                <div class="post-card" style="background-image: url('${blogPosts[i]._embedded["wp:featuredmedia"]["0"].source_url}')">
+                                  <div class="posts-card-background">
+                                    <h2>${blogPosts[i].title.rendered}</h2>
+                                    <h3>${date}</h3>
+                                  </div>
+                                </div>
+                                </a>
+                                `;
+      }
+    }
   } catch (error) {
     console.log(error);
-
     errorMessage("error", error, ".error-message");
   }
 }
 
 getLatestPosts();
 
-// Display first 4 latest posts
-function firstPostsSlider(posts) {
-  for (let i = 0; i < posts.length; i++) {
-    const date = new Date(posts[0].date).toLocaleDateString("utc", {
-      year: "numeric",
-      month: "long",
-      day: "2-digit",
-    });
+// Karusell
+let slideIndex = 1;
+changeSlides(slideIndex);
 
-    blogGrid.innerHTML += `
-                            <a href="blogpost.html?id=${posts[i].id}">
-                            <div class="post-card" style="background-image: url('${posts[i]._embedded["wp:featuredmedia"]["0"].source_url}')">
-                              <div class="posts-card-background">firstSlidePosts
-                                <h2>${posts[i].title.rendered}</h2>
-                                <h3>${date}</h3>
-                              </div>
-                            </div>
-                            </a>
-                        `;
-  }
+// Next/previous controls
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+prevBtn.addEventListener("click", prevSlide);
+nextBtn.addEventListener("click", nextSlide);
+
+function prevSlide() {
+  changeSlides((slideIndex += -1));
 }
 
-// Display next 4 latest posts
+function nextSlide() {
+  changeSlides((slideIndex += 1));
+}
 
-function secondPostsSlider(posts) {}
+// To get the dots work as controls
+function currentSlide(n) {
+  changeSlides((slideIndex = n));
+}
+
+function changeSlides(n) {
+  let i;
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("active", "");
+  }
+  slides[slideIndex - 1].style.display = "grid";
+  dots[slideIndex - 1].className += " active";
+}

5d2ea16bff6c916d593daeda79eb283242ee808a
mathildeew <mathildewiik@hotmail.com>
2022-09-12 16:42:12 +0200

    categories

--- js/blog.js
+++ js/blog.js
@@ -8,12 +8,12 @@ const postsNav = document.querySelector(".blogposts-nav");
 
 document.title += " | Blog";
 
-const baseUrl =
-  "https://projects.mathildeelinor.no/wp-json/wp/v2/posts/?per_page=100&_embed";
+const baseUrl = "https://projects.mathildeelinor.no/wp-json/wp/v2/";
+const blogPostsUrl = "posts/?per_page=100&_embed";
 
 async function getBlogPosts() {
   try {
-    const allBlogPosts = await (await fetch(baseUrl)).json();
+    const allBlogPosts = await (await fetch(baseUrl + blogPostsUrl)).json();
     const categories = await (await fetch(baseUrl + "categories")).json();
     loader.style.display = "none";
 
@@ -31,10 +31,10 @@ getBlogPosts();
 
 // Display blogposts
 let pageIndex = 0;
-let postsPerPage = 4;
+let postsPerPage = 10;
 
-function displayBlogPosts(blogPosts) {
-  console.log(blogPosts);
+function displayBlogPosts(posts) {
+  console.log(posts);
 
   blogPostContainer.innerHTML = "";
 
@@ -43,11 +43,11 @@ function displayBlogPosts(blogPosts) {
     i < pageIndex * postsPerPage + postsPerPage;
     i++
   ) {
-    if (!blogPosts[i]) {
+    if (!posts[i]) {
       break;
     }
 
-    const date = new Date(blogPosts[0].date).toLocaleDateString("utc", {
+    const date = new Date(posts[0].date).toLocaleDateString("utc", {
       year: "numeric",
       month: "long",
       day: "2-digit",
@@ -55,10 +55,10 @@ function displayBlogPosts(blogPosts) {
 
     const blogPostContent = document.createElement("div");
     blogPostContent.innerHTML = `
-    <a href="blogpost.html?id=${blogPosts[i].id}">
-    <div class="post-card" style="background-image: url('${blogPosts[i]._embedded["wp:featuredmedia"]["0"].source_url}')">
+    <a href="blogpost.html?id=${posts[i].id}">
+    <div class="post-card" style="background-image: url('${posts[i]._embedded["wp:featuredmedia"]["0"].source_url}')">
       <div class="posts-card-background">
-        <h2>${blogPosts[i].title.rendered}</h2>
+        <h2>${posts[i].title.rendered}</h2>
         <h3>${date}</h3>
       </div>
     </div>
@@ -70,19 +70,19 @@ function displayBlogPosts(blogPosts) {
 }
 
 // Navigation between blogposts
-function loadBlogPostNav(blogPosts) {
+function loadBlogPostNav(posts) {
   postsNav.innerHTML = "";
 
-  for (let i = 0; i < blogPosts.length / postsPerPage; i++) {
+  for (let i = 0; i < posts.length / postsPerPage; i++) {
     const span = document.createElement("span");
     span.innerHTML = i + 1;
     span.addEventListener("click", (e) => {
       pageIndex = e.target.innerHTML - 1;
-      displayBlogPosts(blogPosts);
+      displayBlogPosts(posts);
     });
 
-    // if (i === pageIndex) {
-    //   span.style.fontSize = "3rem";
+    // if (i == pageIndex) {
+    //   span.style.textDecoration = "underline";
     // }
 
     postsNav.append(span);

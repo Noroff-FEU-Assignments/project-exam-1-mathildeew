8a5e3c0e31b1b088677bffa7abfcd5efea7f9650
mathildeew <mathildewiik@hotmail.com>
2022-09-30 18:36:39 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
-// import { errorMessage } from "./functions/errorMessage.js";
-// import { displayLatestPosts } from "./displayPosts/displayLatestPosts.js";
-// import { carousel } from "./functions/carousel.js";
+import { errorMessage } from "./functions/errorMessage.js";
+import { displayLatestPosts } from "./displayPosts/displayLatestPosts.js";
+import { carousel } from "./functions/carousel.js";
 
-// const loader = document.querySelector(".loader");
+const loader = document.querySelector(".loader");
 
-// const baseUrl = "https://projects.mathildeelinor.no/wp-json/wp/v2/posts";
-// const latestPost = baseUrl + "?per_page=12&_embed";
+const baseUrl = "https://projects.mathildeelinor.no/wp-json/wp/v2/posts";
+const latestPost = baseUrl + "?per_page=12&_embed";
 
-// // Fetch & display latest 12 posts
-// async function getLatestPosts() {
-//   try {
-//     const blogPosts = await (await fetch(latestPost)).json();
-//     loader.style.display = "none";
+// Fetch & display latest 12 posts
+async function getLatestPosts() {
+  try {
+    const blogPosts = await (await fetch(latestPost)).json();
+    loader.style.display = "none";
 
-//     displayLatestPosts(blogPosts);
+    displayLatestPosts(blogPosts);
 
-//     carousel();
-//   } catch (error) {
-//     console.log(error);
-//     errorMessage("error", error, ".error-message");
-//   }
-// }
+    carousel();
+  } catch (error) {
+    console.log(error);
+    errorMessage("error", error, ".error-message");
+  }
+}
 
-// getLatestPosts();
+getLatestPosts();

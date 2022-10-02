c1dedc541044bc65d15897248aae90542b6b242e
mathildeew <mathildewiik@hotmail.com>
2022-10-02 13:57:52 +0200

    arrow function

--- js/functions/imgModal.js
+++ js/functions/imgModal.js
@@ -4,21 +4,20 @@ export function biggerImage() {
 
   //  Open image model when an image on blogpost is clicked
   smallImg.forEach((bigImg) => {
-    bigImg.addEventListener("click", (event) => {
+    bigImg.addEventListener("click", () => {
       imgModal.style.display = "flex";
       body.style.overflow = "hidden";
       imgModal.innerHTML += `
                               <img src="${event.target.src}"/>
                               <p>Close</p>
                             `;
-
-      // Close the image modal
-      imgModal.addEventListener("click", closeImg);
-      function closeImg() {
-        imgModal.style.display = "none";
-        body.style.overflow = "initial";
-        imgModal.innerHTML = "";
-      }
     });
+  });
+
+  // Close the image modal
+  imgModal.addEventListener("click", () => {
+    imgModal.style.display = "none";
+    body.style.overflow = "initial";
+    imgModal.innerHTML = "";
   });
 }
